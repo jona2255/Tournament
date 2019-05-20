be1c43c6a9a60d136feb96f5ce97fc1ab0d4d792
jona2255 <piscis22>
2019-05-20 17:15:48 +0200

    Menus

--- src/com/company/Config.java
+++ src/com/company/Config.java
@@ -5,9 +5,11 @@ import java.io.File;
 public class Config {
 
     public static File teamsFile = new File("teams.txt");
-    public static String participantsFile = "participants.txt";
+    public static File participantsFile = new File ("participants.txt");
 
-    public String ResultFile = "results.txt";
+    public static File resultFile = new File("results.txt");
+    public static File clasificiationFile = new File("clasifications.txt");
+
 
     public static String SEP = ":";
 
